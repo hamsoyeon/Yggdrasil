b55c35bccb3b22f444cad94a75f6dba4a0e81681
roka0105 <cocapepsi123@daum.net>
2022-08-12 17:29:08 +0900

    animation 스크립트 중복 제거

--- Assets/Scripts/Manager/AnimationManager.cs
+++ Assets/Scripts/Manager/AnimationManager.cs
@@ -2,8 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationManager
+public class AnimationManager: MonoBehaviour
 {
+    
+    //Animator ani;
+    //AnimatorController  
+
+    private static AnimationManager instance;
+
+    public static AnimationManager GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = FindObjectOfType<AnimationManager>();
+            if(instance == null)
+            {
+                GameObject container = new GameObject("AnimationManager");
+                instance = container.AddComponent<AnimationManager>();
+            }
+        }
+        return instance;
+    }
+
+    public void PlayAnimation(Animator _ani, string name)
+    {
+        _ani.Play(name);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     public void ChangeAnimationState(Animator animator, string newState, string currentState)
