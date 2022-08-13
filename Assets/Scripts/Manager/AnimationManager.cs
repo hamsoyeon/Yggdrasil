d5ed12544b5d611a7424bd998ec05a38a039c42d
rilphy <wuman130@gmail.com>
2022-08-13 14:24:14 +0900

    animationManager 주석추가

--- Assets/Scripts/Manager/AnimationManager.cs
+++ Assets/Scripts/Manager/AnimationManager.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class AnimationManager: MonoBehaviour
 {
-    
-    //Animator ani;
-    //AnimatorController  
+    //사용범 : 타 스크립트에서 개체 참조 없이 AnimationManager.GetInstance()로 접근해서
+    //PlayAnimation(0 함수를 실행합니다. 
+    //애니메이션을 재생 시킬 모델에 있는 Animator와 그 state 이름을 매개변수에 기입합니다.
+    //스킬 사용시 출력하는 애니메이션 등 간단한 사용에 적합하며
+    //플레이어 이동, 죽음 등 상태와 긴밀하게 연결된 애니메이션 사용엔 부적합 합니다.
 
     private static AnimationManager instance;
 
