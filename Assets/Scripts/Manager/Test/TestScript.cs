39950f17ee7c59f4df38986ff0f4c1e146974e17
rilphy <wuman130@gmail.com>
2022-08-26 13:56:05 +0900

    몬스터 회전 테스트

--- Assets/Scripts/Manager/Test/TestScript.cs
+++ Assets/Scripts/Manager/Test/TestScript.cs
@@ -11,14 +11,37 @@ public class TestScript : MonoBehaviour
 
     //오브젝트(모델) 가져오기
     public GameObject obj;
+    public GameObject fireDemon;
+    public GameObject player;
     Animator anim;
 
     //오브젝트의 애니메이터
     private void Awake()
     {
         anim = obj.GetComponentInChildren<Animator>();
+        fireDemon = GameObject.Find("Fire Demon-Orange");
+        player = GameObject.Find("FallenAngel02");
         //awake 밖에서 하면 오류난다. 유니티 문제는 아니고 C# 문법
     }
+    private void FixedUpdate()
+    {
+        MonsterDirection(fireDemon, player);
+
+    }
+    public float rotateSpeed = 10.0f;
+    void MonsterDirection(GameObject obj)
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+            Quaternion.LookRotation(obj.transform.position),
+            Time.deltaTime * rotateSpeed);
+    }
+    void MonsterDirection(GameObject obj, GameObject plr)
+    {
+        obj.transform.rotation = Quaternion.Lerp(obj.transform.rotation,
+            Quaternion.LookRotation(plr.transform.position),
+            Time.deltaTime * rotateSpeed);
+    }
+
 
     private void Update()
     {
