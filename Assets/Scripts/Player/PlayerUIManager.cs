d7b664e50fd611bfa0eed42673fa3335a1aeb63b
MikangMark <kimjh741963@hanmail.net>
2022-09-05 16:50:07 +0900

    [UIScene,MainScene] UI수정

--- Assets/Scripts/Player/PlayerUIManager.cs
+++ Assets/Scripts/Player/PlayerUIManager.cs
@@ -18,6 +18,10 @@ public class PlayerUIManager : MonoBehaviour
 
     public GameObject minimap_2DIcon_Player;
 
+    public Color m_ATK_Spirit;
+    public Color m_DEF_Spirit;
+    public Color m_SUP_Spirit;
+
     [SerializeField]
     private int m_SkillCount = 6;
 
@@ -28,6 +32,7 @@ public class PlayerUIManager : MonoBehaviour
         minimap_2DIcon_Player.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
         minimap_2DIcon_Player.transform.position = GameObject.FindGameObjectWithTag("Player").transform.position + (Vector3.up * 10);
         GameObject.FindGameObjectWithTag("Player").layer = 14;
+        
         for (int i = 0; i < m_SkillCount; i++)
         {
             Button child = Instantiate(p_spiritSkillBtn);
@@ -37,6 +42,35 @@ public class PlayerUIManager : MonoBehaviour
             child.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255, 255);
             child.GetComponent<Image>().sprite = buttonTexture;
             spirit_Buttons.Add(child);
+            ColorBlock colorBlock = spirit_Buttons[i].colors;
+
+            //(r, g, b, a) 기준 빨간색으로 normal Color 지정
+            switch (i % 3)
+            {
+                
+                case 0:
+                    colorBlock.normalColor = new Color(m_ATK_Spirit.r, m_ATK_Spirit.g, m_ATK_Spirit.b);
+                    colorBlock.highlightedColor = new Color(m_ATK_Spirit.r, m_ATK_Spirit.g, m_ATK_Spirit.b);
+                    colorBlock.pressedColor = new Color(m_ATK_Spirit.r, m_ATK_Spirit.g, m_ATK_Spirit.b);
+                    colorBlock.selectedColor = new Color(m_ATK_Spirit.r, m_ATK_Spirit.g, m_ATK_Spirit.b);
+                    break;
+                case 1:
+                    colorBlock.normalColor = new Color(m_DEF_Spirit.r, m_DEF_Spirit.g, m_DEF_Spirit.b);
+                    colorBlock.highlightedColor = new Color(m_DEF_Spirit.r, m_DEF_Spirit.g, m_DEF_Spirit.b);
+                    colorBlock.pressedColor = new Color(m_DEF_Spirit.r, m_DEF_Spirit.g, m_DEF_Spirit.b);
+                    colorBlock.selectedColor = new Color(m_DEF_Spirit.r, m_DEF_Spirit.g, m_DEF_Spirit.b);
+                    break;
+                case 2:
+                    colorBlock.normalColor = new Color(m_SUP_Spirit.r, m_SUP_Spirit.g, m_SUP_Spirit.b);
+                    colorBlock.highlightedColor = new Color(m_SUP_Spirit.r, m_SUP_Spirit.g, m_SUP_Spirit.b);
+                    colorBlock.pressedColor = new Color(m_SUP_Spirit.r, m_SUP_Spirit.g, m_SUP_Spirit.b);
+                    colorBlock.selectedColor = new Color(m_SUP_Spirit.r, m_SUP_Spirit.g, m_SUP_Spirit.b);
+                    break;
+            }
+
+            
+
+            spirit_Buttons[i].colors = colorBlock;
 
         }
         
