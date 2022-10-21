a75c69e2f5388395cf67f0e57aa2d98b53581307
JangHan Kim <kimjh741963@hanmail.net>
2022-10-21 18:57:22 +0900

    UIScene 서버분리작업중

---
+++ Assets/Scripts/Server+Client_Yeram/UnAbleServer/NewRoomManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class NewRoomManager : Singleton_Ver2.Singleton<NewRoomManager>
+{
+    public GameObject MapPannel;
+
+    [SerializeField]
+    private GameObject selectMap_Content;
+
+    [SerializeField]
+    private Button start_Btn;
+
+    public Button map_Change_Btn;
+    [SerializeField]
+    private Button p_MapBtn;
+    [SerializeField]
+    private int map_Count;
+    [SerializeField]
+    private Sprite[] map_Sprit;
+    [SerializeField]
+    private Image map_View;
+    [SerializeField]
+    private Image room_Map_View;
+    [SerializeField]
+    private int m_MapNum;
+
+    [SerializeField]
+    private List<Button> m_SelectMap_Btn;
+
+    [SerializeField]
+    private Button[] m_SelectChar_Btn;
+
+    [SerializeField]
+    private PlayerSlot m_player_slot;//1 개만있으면됨
+
+    [SerializeField]
+    private GameObject parent_render_Char;
+
+    MapSlot m_curmap;
+    private void Start()
+    {
+        MapPannel.SetActive(false);
+        Init_Map();
+        Select_Map_Btn();
+    }
+
+    // Start is called before the first frame update
+    public void OnClick_Map()
+    {
+        MapPannel.SetActive(true);
+    }
+    public void OnClick_ClosedMap()
+    {
+        MapPannel.SetActive(false);
+    }
+    public void OnClick_Decide_Map(int mapNum)
+    {
+        room_Map_View.sprite = m_SelectMap_Btn[mapNum].transform.GetChild(0).GetComponent<Image>().sprite;
+        m_curmap = m_SelectMap_Btn[mapNum].GetComponent<MapSlot>();
+    }
+    public void Select_Map_Btn()
+    {
+        for (int i = 0; i < map_Count; i++)
+        {
+            Debug.Log(i);
+            int temp = i;
+            m_SelectMap_Btn[temp].onClick.AddListener(() => Map_ViewChange(temp));
+            m_SelectMap_Btn[temp].onClick.AddListener(() => OnClick_Decide_Map(temp));
+        }
+    }
+    public void Map_ViewChange(int _mapNum)
+    {
+        map_View.sprite = m_SelectMap_Btn[_mapNum].transform.GetChild(0).GetComponent<Image>().sprite;
+        m_MapNum = _mapNum;
+    }
+    public void Init_Map()
+    {
+        for (int i = 0; i < map_Count; i++)
+        {
+            m_SelectMap_Btn.Add(Instantiate(p_MapBtn));
+            m_SelectMap_Btn[i].transform.parent = selectMap_Content.transform;
+            m_SelectMap_Btn[i].transform.GetChild(0).GetComponent<Image>().sprite = map_Sprit[i];
+            m_SelectMap_Btn[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = map_Sprit[i].name;
+            m_SelectMap_Btn[i].gameObject.AddComponent<MapSlot>().__Initialize(i);
+        }
+
+    }
+    
+}
