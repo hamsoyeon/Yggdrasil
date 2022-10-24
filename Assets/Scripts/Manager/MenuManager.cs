6e1aa4061ec62b7fc712dc6c2e3aeada9e5c9c30
Jiny <jiny9789@gmail.com>
2022-10-24 18:25:25 +0900

    UI fadeIn/Out

--- Assets/Scripts/Manager/MenuManager.cs
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,15 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     //public static MenuManager instance;
+
+    public class GlobalValue
+    {
+        public enum Transition
+        {
+            Fade,
+        }
+    }
+
 
     enum ShowMenu { WIN = 1, LOSE }
 
@@ -19,7 +29,6 @@ public class MenuManager : MonoBehaviour
 
     public string thisScene;
     public string endingScene = "EndingScene";
-
 
     //private void Awake()
     //{
@@ -57,9 +66,11 @@ public class MenuManager : MonoBehaviour
         thisScene = SceneManager.GetActiveScene().name; //현재 씬 이름을 가져옴
         LosePanel = GameObject.Find("UICanvas").transform.Find("LosePanel").gameObject;
 
+
+
     }
 
-   
+
 
 
 
@@ -74,16 +85,43 @@ public class MenuManager : MonoBehaviour
 
     }
 
+
+    CanvasGroup img;
+    RectTransform rt;
+
+
+
     public void ShowLoseMenu()
     {
         if (WinPanel.activeSelf)
             return;
 
+
         //LosePanel = GameObject.Find("UICanvas").transform.Find("LosePanel").gameObject;
         //Time.timeScale = 0f;
         LosePanel.SetActive(true);  //패배했을때의 패널을 보여주고
+
+
+
+        rt = LosePanel.GetComponent<RectTransform>();
+        rt.DOScale(1, 2f);
+
+        img = LosePanel.GetComponent<CanvasGroup>();//.alpha;
+        img.DOFade(1, 2f)
+            //.SetUpdate(true);   // Unity TimeScale에 영향받지않고 Dotween 내부적으로 체크할수있게 설정을 변경하는 기능... 이렇게 사용하면 위에서 Time.timeScale이 0이 되어도 Dotween이 돌아간다!!!   
+            .OnComplete(TweenComplete);  //위에 DOFade가 완료되면 실행될 함수. 
+
         MinimapPanel.SetActive(false);//미니맵 끄기
     }
+
+    void TweenComplete()
+    {
+        Debug.Log("트윈함수 완료");
+        Time.timeScale = 0f;
+
+    }
+
+
 
     public void ShowWinMenu()
     {
@@ -91,21 +129,32 @@ public class MenuManager : MonoBehaviour
             return;
 
         Time.timeScale = 0f;
+        WinPanel.SetActive(true);
+
+
+        rt = LosePanel.GetComponent<RectTransform>();
+        rt.DOScale(1, 2f);
+
+        //CanvasGroup 넣어주기...
+        var img = LosePanel.GetComponent<CanvasGroup>();//.alpha;
+        img.DOFade(1, 2f)
+            .SetUpdate(true); 
 
         //WinPanel = GameObject.Find("UICanvas").transform.Find("WinPanel").gameObject;
-        WinPanel.SetActive(true);
+
         MinimapPanel.SetActive(false);//미니맵 끄기
     }
 
     public void ReStart()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadSceneAsync(thisScene);
+        SceneManager.LoadScene(thisScene);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void NextStage()
     {
-        SceneManager.LoadSceneAsync(endingScene);
+        SceneManager.LoadScene(endingScene);
     }
 
     public void GameEnd()
