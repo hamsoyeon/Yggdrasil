20f4f4521ea5f2ff6a782c627a7f53838ef74411
JangHan Kim <kimjh741963@hanmail.net>
2022-10-27 00:03:35 +0900

    UIScene수정중

--- Assets/Scripts/Server+Client_Yeram/UnAbleServer/SelectChar.cs
+++ Assets/Scripts/Server+Client_Yeram/UnAbleServer/SelectChar.cs
@@ -14,25 +14,46 @@ public class SelectChar : MonoBehaviour
     string[] char_name;
     [SerializeField]
     TextMeshProUGUI char_Name_Text;
+    [SerializeField]
+    Image[] skill_Img;
+    [SerializeField]
+    int char_Count;
+    Image select_Img;
     private void Start()
     {
-        target = 0;
-        string temp = file_name + target;
-        charView.texture = Resources.Load<RenderTexture>(temp);
-        char_Name_Text.text = char_name[0];
+        Setting_Char(0);
     }
-    public void OnClickChar_0()
+    
+    public void Setting_Char(int target)
     {
-        target = 0;
         string temp = file_name + target;
         charView.texture = Resources.Load<RenderTexture>(temp);
-        char_Name_Text.text = char_name[0];
+        char_Name_Text.text = char_name[target];
+        string temp2 = "Character_" + target + "_Img";
+        select_Img = GameObject.Find(temp2).GetComponent<Image>();
+        select_Img.color = Color.gray;
+        for (int i = 0; i < skill_Img.Length; i++)
+        {
+            string skillAds = "Imgae/Skill_" + target + "_" + i;
+            skill_Img[i].sprite = Resources.Load<Sprite>(skillAds);
+        }
     }
-    public void OnClickChar_1()
+    public void OnClickRightBtn()
     {
-        target = 1;
-        string temp = file_name + target;
-        charView.texture = Resources.Load<RenderTexture>(temp);
-        char_Name_Text.text = char_name[1];
+        if (target < char_Count - 1)
+        {
+            target++;
+            Setting_Char(target);
+        }
+        
+    }
+    public void OnClickLeftBtn()
+    {
+        if (target > 0)
+        {
+            target--;
+            Setting_Char(target);
+        }
+        
     }
 }
